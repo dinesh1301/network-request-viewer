78e71b855d3f4bdb2fd458fe210f21cfbc5e5a74
Dinesh Singh <dineshsingh@mindtickle.coml>
2019-11-06 22:54:22 +0530

    removed shakedetector

--- networkloggerlibrary/src/main/java/com/dinesh/networklogger/NetworkLoggerProvider.kt
+++ networkloggerlibrary/src/main/java/com/dinesh/networklogger/NetworkLoggerProvider.kt
@@ -3,13 +3,8 @@ package com.dinesh.networklogger
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.util.Log
 import androidx.room.Room
-import retrofit2.Retrofit
-import safety.com.br.android_shake_detector.core.ShakeCallback
-import safety.com.br.android_shake_detector.core.ShakeOptions
-import safety.com.br.android_shake_detector.core.ShakeDetector
+
 
 
 
